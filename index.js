0c6df2f6bda5686b7f38fb90c95d7dc863172c13
lab <lab@lab.myguest.virtualbox.org>
2025-04-27 23:21:22 +0200

    login

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const env = nunjucks.configure('views', {
     express: app,
 });
 
+// Middleware para procesar datos del cuerpo de las solicitudes
+app.use(express.urlencoded({ extended: true })); // Para datos de formularios
+app.use(express.json()); // Para datos en formato JSON
+
+
 app.set('view engine', 'njk'); // Configuramos Nunjucks como motor de plantillas
 
 app.get('/', async (req, res) => {
@@ -46,6 +51,40 @@ app.get('/tabladb', async (req, res) => {
         users
     });
 });
+
+app.get('/tabladb', async (req, res) => {
+    const usersRaw = await User.findAll(); 
+    const users = usersRaw.map(user => {
+        return {
+            id: user.id,
+            name: user.username,
+            password: user.password, 
+        };
+    });
+    console.log(users);
+    res.render('database', { 
+        title: 'Tabla de Usuarios',
+        desc: 'Lista de usuarios registrados en la base de datos:',
+        users
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', {
+        title: 'Iniciar Sesión',
+        desc: 'Por favor, ingresa tu nombre de usuario y contraseña para registrarte.'
+    });
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    await User.create({ username, password });
+    console.log(`Usuario registrado: ${username}`);
+        
+    // Redirigir a la tabla de usuarios después de registrar
+    res.redirect('/tabladb');
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
